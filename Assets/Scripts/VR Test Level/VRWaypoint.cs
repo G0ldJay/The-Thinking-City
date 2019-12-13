f3d7877db36c759e63617e58bf34d5bd41941991
Slamacy <oisinmurphy598@gmail.com>
2019-12-13 01:13:09 +0000

    VR Test level waypoint system implemented

---
+++ Assets/Scripts/VR Test Level/VRWaypoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRWaypoint : MonoBehaviour {
+    public VRWaypointSystem VRWPS;
+
+    private void Awake() {
+        this.VRWPS = FindObjectOfType<VRWaypointSystem>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag != "Player") return;
+        Debug.Log("Destroy waypoint");
+        // laod next waypoint then destroy parent
+        VRWPS.LoadNextWaypoint();
+        Destroy(transform.parent.gameObject);
+        
+    }
+}
---
+++ Assets/Scripts/VR Test Level/VRWaypointSystem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class VRWaypointSystem : MonoBehaviour {
+
+    public GameObject[] waypointMarkers = new GameObject[11];
+    public GameObject waypointPrefab;
+    private int waypointIndex = 1;
+
+    public void LoadNextWaypoint() {
+        Vector3 position = this.waypointMarkers[this.waypointIndex].transform.position + new Vector3(0, 1.91f, 0);
+        GameObject newWaypoint = Instantiate(this.waypointPrefab, position, Quaternion.identity);
+        this.waypointIndex++;
+
+        if (waypointIndex == 11) {
+            this.waypointIndex = 0;
+            //LoadNextWaypoint();
+        }
+    }
+}
