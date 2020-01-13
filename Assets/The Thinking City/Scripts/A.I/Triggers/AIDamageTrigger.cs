e63bc712965e4d1baee6b10cdc771570d556bd7f
GoldJay <jasonlynch10@hotmail.com>
2020-01-13 17:17:21 +0000

    Added damage triggers to AI so it registers hitting player. Also added in trigger curves to animations to specify when in animation the AI can do damage

---
+++ Assets/The Thinking City/Scripts/A.I/Triggers/AIDamageTrigger.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIDamageTrigger : MonoBehaviour
+{
+    [SerializeField] string _parameter = "";
+    [SerializeField] int _bloodParticlesBurstAmount = 10;
+
+    private AIStateMachine _stateMachine = null;
+    private Animator _animator = null;
+    private int _parameterHash = -1;
+    // private bool _attacked = false;
+
+    private void Start()
+    {
+        _stateMachine = transform.root.GetComponentInChildren<AIStateMachine>();
+
+        if (_stateMachine != null) _animator = _stateMachine.animator;
+
+        _parameterHash = Animator.StringToHash(_parameter);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!_animator) return;
+
+        if (other.gameObject.CompareTag("Player") && _animator.GetFloat(_parameterHash) > 0.9f)
+        {
+            Debug.Log("Player hit by " + _parameter + "!");
+
+            if (GameManager.instance && GameManager.instance.bloodParticles)
+            {
+                ParticleSystem system = GameManager.instance.bloodParticles;
+                system.transform.position = transform.position;
+                system.transform.rotation = Camera.main.transform.rotation;
+
+                var main = system.main;
+                main.simulationSpace = ParticleSystemSimulationSpace.World;
+
+                system.Emit(_bloodParticlesBurstAmount);
+
+            }
+            //_attacked = !_attacked;
+        }
+
+    }
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    _attacked = !_attacked;
+    //}
+}
+
