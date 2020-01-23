e7928e628507dadcc4d65203a93384eebf2506c5
Slamacy <oisinmurphy598@gmail.com>
2020-01-23 07:03:25 +0000

    lever edits

---
+++ Assets/The Thinking City/Scripts/VR Objects/LeverAction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverAction : MonoBehaviour {
+    
+    public void OnMinAngle() {
+        Debug.Log("Lever Min Hit!");
+    }
+
+    public void OnMaxAngle() {
+        Debug.Log("Lever Max Hit!");
+    }
+
+}
