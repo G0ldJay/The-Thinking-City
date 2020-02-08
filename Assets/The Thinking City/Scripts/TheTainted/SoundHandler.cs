939f4b253d7062d87d95a09936c181ca4a4fe260
Eoghan Maguire <eoghanmaguire1@gmail.com>
2020-02-08 21:47:12 +0000

    Cutoff fixed for robot steel step.

--- Assets/The Thinking City/Scripts/TheTainted/SoundHandler.cs
+++ Assets/The Thinking City/Scripts/TheTainted/SoundHandler.cs
@@ -14,17 +14,14 @@ public class SoundHandler : MonoBehaviour {
     public void MeatFoot() {
         //play sound at left foot transform
 
-        //Debug.Log("PLAY MEATY FOOT SOUND");
         FMODUnity.RuntimeManager.PlayOneShot("event:/RobotMeatStep", MeatFootTransform.transform.position);
     }
 
     //Play Steel foot sound from foot audio source
     public void MetalFoot() {
-
-        FMODUnity.RuntimeManager.PlayOneShot("event:/RobotMetalStep", MetalFootTransform.transform.position);
-
         //play sound at right foot transform
-        //Debug.Log("PLAY METAL FOOT SOUND");
+
+        FMODUnity.RuntimeManager.PlayOneShot("event:/RobotMetalStep", MetalFootTransform.transform.position);;
     }
 
 }
