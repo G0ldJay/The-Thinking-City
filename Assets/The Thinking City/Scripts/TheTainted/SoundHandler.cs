6d957a4228ae84a5f7a9a642ef08b4e13a4ecd88
Slamacy <oisinmurphy598@gmail.com>
2020-02-08 20:01:36 +0000

    Added Fully textured Tainted with new anims (issue with < 90 deg turn), & set up footstep sound scripts to work with walk animation events

---
+++ Assets/The Thinking City/Scripts/TheTainted/SoundHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundHandler : MonoBehaviour {
+
+    public Transform MeatFootTransform;
+    public Transform MetalFootTransform;
+
+    //Play Meaty foot sound from foot audio source
+    public void MeatFoot() {
+        //play sound at left foot transform
+        Debug.Log("PLAY MEATY FOOT SOUND");
+    }
+
+    //Play Steel foot sound from foot audio source
+    public void MetalFoot() {
+        //play sound at right foot transform
+        Debug.Log("PLAY METAL FOOT SOUND");
+    }
+
+}
