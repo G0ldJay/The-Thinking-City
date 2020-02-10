743f1960e92d46ad4504515c61dd4330ae47b6aa
Eoghan Maguire <eoghanmaguire1@gmail.com>
2020-02-10 18:47:42 +0000

    Sound event changes to Robotic Arm.

--- Assets/The Thinking City/Scripts/SoundHandlerRoboticArm.cs
+++ Assets/The Thinking City/Scripts/SoundHandlerRoboticArm.cs
@@ -6,14 +6,17 @@ public class SoundHandlerRoboticArm : MonoBehaviour {
     FMOD.Studio.EventInstance soundEvent;
 
     public void PlayServos() {
-        //soundEvent = FMODUnity.RuntimeManager.CreateInstance("event:/RobotArmServo");
-        //FMODUnity.RuntimeManager.AttachInstanceToGameObject(soundEvent, GetComponent<Transform>(), GetComponent<Rigidbody>());
-        FMODUnity.RuntimeManager.PlayOneShot("event:/RobotArmServo", GetComponent<Transform>().position);
+        soundEvent = FMODUnity.RuntimeManager.CreateInstance("event:/RobotArmServo");
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(soundEvent, GetComponent<Transform>(), GetComponent<Rigidbody>());
+        soundEvent.start();
+        // FMODUnity.RuntimeManager.PlayOneShot("event:/RobotArmServo", GetComponent<Transform>().position);
 
     }
 
     public void StopServos() {
         soundEvent.release();
+        FMOD.Studio.STOP_MODE stop = FMOD.Studio.STOP_MODE.IMMEDIATE;
+        soundEvent.stop(stop);
     }
 
 }
