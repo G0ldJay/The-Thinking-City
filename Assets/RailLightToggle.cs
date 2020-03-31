7229ef9e232208dcd9162a31a7d0af0342002103
Slamacy <oisinmurphy598@gmail.com>
2020-03-31 04:24:08 +0100

    Rail now highlights progress made with robotic arm

---
+++ Assets/RailLightToggle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailLightToggle : MonoBehaviour {
+    [SerializeField]
+    private bool UseMaterials = false;
+    [SerializeField]
+    private bool StartActive  = false;
+    [SerializeField]
+    private Material offMaterial;
+    [SerializeField]
+    private Material onMaterial;
+
+    private void Start() {
+        ToggleEmissive(StartActive);
+    }
+
+    // if true, turn emissive on, else turn off
+    public void ToggleEmissive(bool On) {
+        if(UseMaterials) {
+            if(On) {
+                GetComponent<MeshRenderer>().material = onMaterial;
+            }
+            else {
+                GetComponent<MeshRenderer>().material = offMaterial;
+            }
+        }
+        else {
+            if (On) {
+                GetComponent<MeshRenderer>().material.EnableKeyword("_EMISSION");
+            }
+            else {
+                GetComponent<MeshRenderer>().material.DisableKeyword("_EMISSION");
+                
+            }
+        }
+
+    }
+}
