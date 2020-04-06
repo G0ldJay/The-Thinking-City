a5dca253f425228b9bdf0605076a2d0bb7df0361
Eoghan Maguire <eoghanmaguire1@gmail.com>
2020-04-06 16:16:35 +0100

    Collider related lines TRISH line overlap fix.

---
+++ Assets/TrishBusHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+
+public class TrishBusHandler : MonoBehaviour
+{
+
+    FMOD.Studio.Bus TrishBus;
+    public FMODUnity.StudioEventEmitter StudioEventEmitter;
+
+    void Start()
+    {
+        TrishBus = FMODUnity.RuntimeManager.GetBus("Bus:/MasterGroup/Trish");
+ 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (StudioEventEmitter.TriggerOnce == false)
+            {
+                TrishBus.stopAllEvents(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            }
+
+            StudioEventEmitter.Play();
+            StudioEventEmitter.TriggerOnce = true;
+        }
+    }
+}
