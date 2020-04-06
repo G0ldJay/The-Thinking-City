8bccdcb4e761f9324e81905fd12092b40d6b3c60
Eoghan Maguire <eoghanmaguire1@gmail.com>
2020-04-06 17:16:11 +0100

    Trish OnEnable Line Overlapping fix.

---
+++ Assets/TrishBusHandlerOnEnable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrishBusHandlerOnEnable : MonoBehaviour
+{
+    FMOD.Studio.Bus TrishBus;
+    public FMODUnity.StudioEventEmitter StudioEventEmitter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TrishBus = FMODUnity.RuntimeManager.GetBus("Bus:/MasterGroup/Trish");
+    }
+
+    private void OnEnable()
+    {
+        if (StudioEventEmitter.TriggerOnce == false)
+        {
+            TrishBus.stopAllEvents(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        }
+
+        StudioEventEmitter.Play();
+        StudioEventEmitter.TriggerOnce = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
