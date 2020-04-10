4c62dc80e34819d23cb5c156057516eabc1fc68d
Eoghan Maguire <eoghanmaguire1@gmail.com>
2020-04-10 16:57:30 +0100

    Cabinet breathing implemented in the VR Closet Prefab.

---
+++ Assets/CabinetBreathingSoundHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class CabinetBreathingSoundHandler : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    bool cabinetDoorsClosed;
+    public StudioEventEmitter studioEventEmitter;
+
+    void Start()
+    {
+        cabinetDoorsClosed = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (cabinetDoorsClosed == false)
+            {
+                studioEventEmitter.Play();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            studioEventEmitter.Stop();
+            cabinetDoorsClosed = true;
+        }
+    }
+
+    void Update()
+    {
+        if (cabinetDoorsClosed == true)
+        {
+            studioEventEmitter.Stop();
+        }
+    }
+}
