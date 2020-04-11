7e48533581b9561cba8a1af1e5a59d7268b93464
GoldJay <jasonlynch10@hotmail.com>
2020-04-11 03:12:55 +0100

    A.I robot turn fixed. Lights added to ending event. Eye animation added. Script for ending event added

---
+++ Assets/The Thinking City/Scripts/Mission/EyeEndingEvent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeEndingEvent : MonoBehaviour
+{
+    [SerializeField] private GameObject _trishEyeAudio      = null;
+    [SerializeField] private Animator   _eyeAnimator        = null;
+    [SerializeField] private Animator   _exitDoorAnimator   = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _trishEyeAudio.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (_trishEyeAudio != null)
+            {
+                _trishEyeAudio.SetActive(true);
+            }
+            if (_eyeAnimator != null)
+            {
+                _eyeAnimator.SetTrigger("OpenEye");
+            }
+        }
+    }
+}
